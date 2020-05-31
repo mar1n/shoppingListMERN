25e5ae2ad9e6d731309a2130c8179f0a0a5c136a
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-31 20:38:58 +0100

    connect to database

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+const db = require('./config/keys').mongoURI
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Mongo connected...'))
+    .catch(error => console.log(error))
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
