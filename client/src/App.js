fa2ab76e7ebc69a81facc2cd5a98ac7e3e8afdbf
mar1n <30738216+mar1n@users.noreply.github.com>
2020-06-05 23:40:49 +0100

    finishing redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import AppNavbar from "./components/AppNavbar";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { Container } from "reactstrap";
 import ShoppingList from "./components/ShoppingList";
+import ItemModal from "./components/ItemModal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -12,7 +13,10 @@ function App() {
     <Provider store={store}>
       <div className="App">
         <AppNavbar />
-        <ShoppingList />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
       </div>
     </Provider>
   );
